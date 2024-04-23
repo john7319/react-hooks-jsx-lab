aa9cbcbf74f7bc91dba6d08662084542d6ef920c
john7319 <jonniekaras@gmail.com>
2024-04-23 16:31:01 +0300

    Added jsx to the components

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { image } from "../data/data";
 
+const AboutElement = (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>This Is My Website</p>
+    <img src= {image} alt="I made this" />
+  </div>
+)
+
 function About() {
-  return <div>About</div>;
+  return <>{AboutElement}</>;
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const HomeElement = (
+    <div id="home">
+      <h1 style={{color: "firebrick"}}>{name} is a Web Developer from {city}</h1>
+
+    </div>
+  )
+  return (<>{HomeElement}</>);
 }
 
 export default Home;
